a45f498e59d21b648a43aae8822bf7cce93d20cd
Nadeem Khedr <nadeemkhedr@gmail.com>
2018-08-16 15:45:02 +0800

    more data for testing

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ const PORT = process.env.PORT || '8000'
  */
 app.get('/hello/:name', (req, res) => {
   const requestId = req.headers['kinto-request-id']
+  // test with normal json logging
   console.log({
     kinto_request_id: requestId,
-    key: 'YEA',
-    value: 'HOLLY'
+    benName: 'BEN',
+    benValue: 'BEN IS GREAT'
   })
+  // test with json stringify
+  console.log(
+    JSON.stringify({
+      kinto_request_id: requestId,
+      benName: 'BEN',
+      benValue: 'BEN IS GREAT'
+    })
+  )
   console.log('All HEADERS:', req.headers)
   res.send({
-    message: `Hello ${process.env.USER_X_NAME}`
+    message: `Hello requestId value: ${requestId}`
   })
 })
 
